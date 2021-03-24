c090f6a144ad78eef053ae4287dfe4a9c1b2983f
Dat Doan <dathuydoan@gmail.com>
2021-03-24 16:09:28 -0700

    Dataset importing

--- Code.py
+++ Code.py
@@ -10,6 +10,18 @@ import rdflib
 
 from rdflib.Graph import Graph
 
+earthquakes = pd.read_csv("earthquakes_data.tsv", sep='\t',header=0,skiprows=1).to_numpy()
+#year = 1
+#month = 2
+#location = 9
+
+permits = pd.read_excel("permitsbyusreg_cust.xls",sheet_name=1,header=0,skiprows=5).to_numpy().T[1:].T[:398]
+#Date = 0
+#Total = 1
+#Northeast = 6
+#Midwest = 8
+#South = 10
+#West = 10
 
 #Creating the graph
 g = Graph()
