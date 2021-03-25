7417e9ed00980746dbd8f249a7c8a0aa17867615
Dat Doan <dathuydoan@gmail.com>
2021-03-24 20:04:33 -0700

    Basic outline for code is done

--- Code.py
+++ Code.py
@@ -26,6 +26,38 @@ permits = pd.read_excel("permitsbyusreg_cust.xls",sheet_name=1,header=0,skiprows
 #Creating the graph
 g = Graph()
 
+#West = 12
+
+n = Namespace("http://web.csulb.edu/~016412147/CECS571/SemanticOntology/")
+g = Graph()
+
+g.bind('earthpermit', n)
+
+#Class Creation
+g.add( (n.Permit, RDFS.subClassOf, OWL.Thing) )
+g.add( (n.Earthquake, RDFS.subClassOf, OWL.Thing) )
+
+#Property Creation
+#permit to month
+g.add( (n.authorizedOn, RDFS.subPropertyOf, OWL.topObjectProperty) )
+#earthquake to month
+g.add( (n.shookOn, RDFS.subPropertyOf, OWL.topObjectProperty) )
+
+g.add( (n.numberAuthorized, RDFS.subPropertyOf, OWL.topDataProperty) )
+g.add( (n.month, RDFS.subPropertyOf, OWL.topDataProperty) )
+g.add( (n.location, RDFS.subPropertyOf, OWL.topDataProperty) )
+
+#sending the data
+for i in range(permits.shape[0]):
+    permitTest = BNode()
+    g.add( (permitTest,RDFS.label,Literal(permits[i][0].date().isoformat()+" Permits")))
+    g.add( (permitTest,n.numberAuthorized,Literal(permits[i][1] * 1000, datatype=XSD.integer))) 
+    g.add( (permitTest,n.authorizedOn,(Literal(permits[i][0].date(), datatype=XSD.date)) ) )
+    
+for i in range(earthquakes.shape[0]):
+    earthquakeTest = BNode()
+    g.add( (earthquakeTest,n.shookOn,(Literal(datetime.date(earthquakes[i][1],earthquakes[i][2],1),  datatype=XSD.date)) ) )
+    g.add( (earthquakeTest,n.location,(Literal(earthquakes[i][9], datatype=XSD.string)) ) )
 
 #Exporting the graph
 g.serialize("test.rdf", format="xml")
